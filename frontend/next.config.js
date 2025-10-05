6cfd1d03e988caad4fff393ee398e2a047a66ac8
kkmtheone <kkm.theone@gmail.com>
2025-10-05 15:31:16 +0300

    Fix next.config.js for static export

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,1 +1,7 @@
-package
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'export', // enables static export so it builds into "out"
+}
+
+module.exports = nextConfig
