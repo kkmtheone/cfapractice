cc7e8c8beaa41ecc742da172be2d42e334abab27
kkmtheone <kkm.theone@gmail.com>
2025-10-06 12:44:46 +0300

    Fix Next.js pages by adding default exports

---
+++ frontend/pages/practice/[topic].js
@@ -0,0 +1,8 @@
+export default function PracticePage() {
+  return (
+    <div>
+      <h1>Practice Page</h1>
+      <p>This will render practice questions by topic.</p>
+    </div>
+  );
+}
---
+++ frontend/pages/quiz/[id].js
@@ -0,0 +1,8 @@
+export default function QuizPage() {
+  return (
+    <div>
+      <h1>Quiz Page</h1>
+      <p>This will render a quiz by ID.</p>
+    </div>
+  );
+}
