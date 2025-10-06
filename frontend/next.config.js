926c43b4cd80b05aa3b6e38e5abb3c704f0283fe
kkmtheone <kkm.theone@gmail.com>
2025-10-06 13:39:22 +0300

    new app using python only

--- frontend/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  output: 'export', // enables static export so it builds into "out"
-}
-
-module.exports = nextConfig
